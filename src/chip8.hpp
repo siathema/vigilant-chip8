099467065a994e2f753cb09d743014dcf00999af
Matthias Linford <nate.holaday@gmail.com>
2016-05-27 21:36:41 -0700

    Added some instruction decoding

--- src/chip8.hpp
+++ src/chip8.hpp
@@ -5,22 +5,7 @@
 class chip8 {
 
   // CHIP8 data registers V0..VF
-  uint16_t v0_reg; 
-  uint16_t v1_reg;
-  uint16_t v2_reg;
-  uint16_t v3_reg;
-  uint16_t v4_reg;
-  uint16_t v5_reg;
-  uint16_t v6_reg;
-  uint16_t v7_reg;
-  uint16_t v8_reg;
-  uint16_t v9_reg;
-  uint16_t va_reg;
-  uint16_t vb_reg;
-  uint16_t vc_reg;
-  uint16_t vd_reg;
-  uint16_t ve_reg;
-  uint16_t vf_reg;
+  uint8_t DATA_reg[16];
 
   // Stack pointer (SP)
   uint8_t SP_reg;
@@ -29,17 +14,14 @@ class chip8 {
   uint16_t PC_reg;
 
   // Pointer to main memory
-  uint16_t *mem;
+  char *mem;
 
 public:
-  chip8(uint8_t* memory);
+  chip8(char * memory);
   ~chip8();
-  void run_instruction(uint16_t opcode);
+  void run_instruction();
   void update();
-}
-
-#endif
-
+};
 
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <stdint.h>
+#include "chip8.hpp"
 
-int main() {
+int main(int argc, char ** argv) {
+  
+  if(argc != 2){
+    std::cout << "usage: disassembler [path to file]" << std::endl;
+    return 0;
+  }
+
+  char * m_pathToFile = argv[1];
+
+  std::streampos m_file_size;
+  char * m_buffer;
+
+  std::ifstream m_fileStream;
+  m_fileStream.open(m_pathToFile, std::ios::in|std::ios::binary|std::ios::ate);
+  if (m_fileStream.is_open()) {
+    m_file_size = m_fileStream.tellg();
+    m_buffer = new char [m_file_size];
+    m_fileStream.seekg(0, std::ios::beg);
+    m_fileStream.read(m_buffer, m_file_size);
+    m_fileStream.close();
+  } else {
+    std::cout << "Cannot open file: " << std::endl;
+    return -1;
+  }
 
   std::cout << "I'm a CHIP8 emulator!" << std::endl;
   return 0;
